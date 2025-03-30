1b8c977dbaa67b6e823065039f7212d66333f1ca
AdityaBhadauria <adityab5262@gmail.com>
2025-03-30 20:17:40 +0530

    initial commit

---
+++ src/main/java/com/aditya/bookstore/service/UserService.java
@@ -0,0 +1,29 @@
+package com.aditya.bookstore.service;
+
+import com.aditya.bookstore.model.User;
+import com.aditya.bookstore.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    private final BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+
+    public User registerUser(User user) {
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        return userRepository.save(user);
+    }
+
+    public User findByEmail(String email) {
+        return userRepository.findByEmail(email).orElse(null);
+    }
+
+
+
+
+}
