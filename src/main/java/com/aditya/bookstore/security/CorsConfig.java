fc990a1f6c061c4a9d28b9d351bc228f4c54bf25
AdityaBhadauria <adityab5262@gmail.com>
2025-04-07 01:16:19 +0530

    loaclhost support

--- src/main/java/com/aditya/bookstore/security/CorsConfig.java
+++ src/main/java/com/aditya/bookstore/security/CorsConfig.java
@@ -14,6 +14,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/api/**")
+
                         .allowedOrigins("http://localhost:3000","https://book-store-frontend-aditya-bhadaurias-projects.vercel.app")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
